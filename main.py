fa5350fae6fb8aa6c386beef989c5c39943b4056
RyanMacMillanSoftware <ryanmacmillan1996@gmail.com>
2024-11-23 12:52:18 +1300

    Update main to have /wines endpoint and create DB tables on startup

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
 
+from db.models import Wine
+from wine_models import WineResponse
+from db.session import get_db 
+from db.models import create_db_tables
+from db.DBEngine import DBEngine
+
+# Instantiate API
 app = FastAPI()
 
+# Create DB tables from ORM schema
+create_db_tables(DBEngine.get_engine())
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Define the GET /wines endpoint
+@app.get("/wines", response_model=WineResponse)
+def get_all_wines(db: Session = Depends(get_db)):
+    wines = db.query(Wine).all()
+    return WineResponse(wines=wines)
